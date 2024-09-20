ed031e95c415ee4cc02497b7fc7cb583dc4fcbaa
scott <scott5497ify@gmail.com>
2024-09-20 23:12:31 +0800

    新增影像裁剪和人物偵測階段，初始化相關參數並清空偵測框；更新主程式以支援新階段

---
+++ main copy.py
@@ -0,0 +1,25 @@
+from pipeline.processing_pipeline import ProcessingPipeline
+from pipeline.stages.deblurring_stage import DeblurringStage
+from pipeline.stages.image_cropping_stage import ImageCroppingStage
+from pipeline.stages.person_detection_stage import PersonDetectionStage
+import time
+import cv2
+
+cap = cv2.VideoCapture(0)
+processing_pipeline = ProcessingPipeline(source=self.source)
+pipelines = [
+    ImageCroppingStage(),  # 黑板先
+    PersonDetectionStage(),
+    DeblurringStage(),
+]
+for stage in pipelines:
+    processing_pipeline.add_stage(stage.__class__.__name__, stage)
+while cap.is_running:
+    ret, frame = cap.read()
+
+    timestamp = time.time()
+
+    # Process the frame using the pipeline
+    frame, data, timestamp = processing_pipeline.process(frame, timestamp)
+    cv2.imshow("test", frame)
+    cv2.waitKey(0)
