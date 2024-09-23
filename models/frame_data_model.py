d13cc7ca07dd755ce97ecd41d26a530c209fd6a4
scott <scott5497ify@gmail.com>
2024-09-23 22:52:44 +0800

    PersonDetectionStage新增FUNCITON與FrameDataModel ADD MEMBER

--- models/frame_data_model.py
+++ models/frame_data_model.py
@@ -2,12 +2,20 @@
 
 from pydantic import BaseModel
 from typing import List, Any
+from ultralytics import YOLOWorld
+import supervision as sv
 
 
 class FrameDataModel(BaseModel):
     timestamp: float
     person_positions: List[Any] = []
-
+    # 初始化YOLO model/分類類別 /偵測到的物件們
+    model: YOLOWorld
+    detection_class: List[str] = []
+    detections: sv.Detections
+    # 單獨物件列表
+    people_boxes: List[Any] = []
+    combined_boxes: List[Any] = []
     # 可以快速添加新的屬性，例如：
     # additional_info: dict = {}
 
--- pipeline/stages/person_detection_stage.py
+++ pipeline/stages/person_detection_stage.py
@@ -6,6 +6,8 @@ from typing import Any, Tuple
 from pipeline import PipelineStage
 from models import FrameDataModel
 import numpy as np
+from ultralytics import YOLOWorld
+import supervision as sv
 
 
 class PersonDetectionStage(PipelineStage):
@@ -32,6 +34,11 @@ class PersonDetectionStage(PipelineStage):
         """
 
         data.person_positions = self.__test_generate_random_positions()
+        data = self.init_object_detection_model(data)  # 初始化YOLO model/分類類別
+        data.detections = self.get_detections(frame, data.model)  # 偵測到的物件們
+        data.people_boxes, data.combined_boxes = self.get_people_boxes(  # 單獨物件列表
+            frame, data.detections
+        )
         return frame, data
 
     def __test_generate_random_positions(self) -> Any:
@@ -43,4 +50,26 @@ class PersonDetectionStage(PipelineStage):
             for _ in range(random.randint(0, 10))
         ]
         return test
-        
+
+    def init_object_detection_model(data: FrameDataModel):
+        data.detection_class = ["person", "blackboard"]
+        data.model = YOLOWorld("yolov8s-world.pt")
+        data.model.set_classes(data.detection_class)
+        return data
+
+    def get_detections(self, frame, model):
+        results = model.predict(frame, conf=0.5)
+        detection = sv.Detections.from_ultralytics(results[0])
+        return detection
+
+    # 把畫面中的人加入=>人列表
+    def annotate_people(self, detection):
+        people_boxes_temp = []
+        combined_boxes_temp = []
+        for i, (box, class_id) in enumerate(zip(detection.xyxy, detection.class_id)):
+            if class_id == 0:  # Person class
+                people_boxes_temp.append(box)
+            elif class_id != 1:  # Exclude blackboard
+                x1, y1, x2, y2 = map(int, box)
+                combined_boxes_temp.append([x1, y1, x2, y2, class_id])
+        return people_boxes_temp, combined_boxes_temp
